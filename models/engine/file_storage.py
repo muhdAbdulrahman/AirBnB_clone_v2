7a668d7ddfe94db36721e2a62a19947b0d3c2ec8
muhdAbdulrahman <132393678+muhdAbdulrahman@users.noreply.github.com>
2024-03-22 21:36:45 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,26 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        res = {}
+        if cls:
+            for key, value in FileStorage.__objects.items():
+                clss_name = key.split('.')
+                if clss_name[0] == cls.__name__:
+                    res[key] = value
+        else:
+            return FileStorage.__objects
+        return res
+
+    def delete(self, obj=None):
+        """Delete obj from __objects"""
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
+                self.save()  # Save the changes after deletion
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -20,8 +37,8 @@ class FileStorage:
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
+            for key, val in FileStorage.__objects.items():
+                # Exclude SQLAlchemy-specific attributes before saving
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
@@ -45,6 +62,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+
+
+ 
